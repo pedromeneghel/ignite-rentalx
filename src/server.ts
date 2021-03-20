5566c1ec43f0ea38a499deb36523e26d4ed5cd9f
Pedro Meneghel <pedromeneghel@gmail.com>
2021-03-20 11:29:03 -0300

    Create categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
+app.use(categoriesRoutes);
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello" });
