844a9f0ddf9e702c0730e5b350a2438f28600653
Pedro Meneghel <pedromeneghel@gmail.com>
2021-03-21 15:59:25 -0300

    Principio de Substituição de Liskov

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,12 +1,12 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
-interface IRequest {
+export interface IRequest {
   name: string;
   description: string;
 }
 
 export class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) { }
+  constructor(private categoriesRepository: ICategoriesRepository) { }
 
   execute({ description, name }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
