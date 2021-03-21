431783afb5a6969e6a057729664b852d37929811
Pedro Meneghel <pedromeneghel@gmail.com>
2021-03-21 09:12:09 -0300

    Lista categorias

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,32 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): Category {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,22 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ description, name });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  return response.status(201).end();
+});
 
-  categories.push(category);
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoriesRepository.list();
 
-  return response.status(201).json(category);
+  return response.status(200).json(categories);
 });
 
 export { categoriesRoutes };
