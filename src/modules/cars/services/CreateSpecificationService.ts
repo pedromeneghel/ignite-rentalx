8f4f600dfd0848f55e31d7496aa5de89eb0d797b
Pedro Meneghel <pedromeneghel@gmail.com>
2021-03-22 16:12:39 -0300

    Criada funcionalidade de especificações

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,22 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+export interface IRequest {
+  name: string;
+  description: string;
+}
+
 export class CreateSpecificationService {
-  execute(): void {
-    console.log("TODO");
+  constructor(private specificationRepository: ISpecificationsRepository) { }
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificationRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists.");
+    }
+
+    this.specificationRepository.create({ description, name });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 app.use(express.json());
 app.use("/categories", categoriesRoutes);
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello" });
-});
-
-app.get("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
