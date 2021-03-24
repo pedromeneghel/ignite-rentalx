8844c5d5eff247d9d82e755c2a030fcdbd0ba276
Pedro Meneghel <pedromeneghel@gmail.com>
2021-03-24 14:43:07 -0300

    Tratando csv durante upload utilizando stream

--- src/modules/cars/useCases/importCategory/ImporteCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImporteCategoryUseCase.ts
@@ -1,5 +1,15 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 export class ImporteCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
